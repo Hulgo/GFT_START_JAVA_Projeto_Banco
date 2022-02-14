9b24b33fbaca2e0539ad6c24bdb0e737c40b7ba9
Hulgo Leonardo J. Andrade <hulgoleo@gmail.com>
2022-02-14 11:46:08 -0300

    Third Commit - Implements Inheritance

--- src/br/dev/hltech/model/ContaCorrente.java
+++ src/br/dev/hltech/model/ContaCorrente.java
@@ -1,32 +1,5 @@
 package br.dev.hltech.model;
 
-public class ContaCorrente {
+public class ContaCorrente extends Conta{
 
-    private int agencia;
-    private int numero;
-    private double saldo;
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void sacar() {
-
-    }
-
-    public void depositar() {
-
-    }
-
-    public void transferir() {
-
-    }
 }
---
+++ src/br/dev/hltech/model/IConta.java
@@ -0,0 +1,10 @@
+package br.dev.hltech.model;
+
+public interface IConta {
+
+    void sacar(double valor);
+
+    void depositar(double valor);
+
+    void transferir(double valor, Conta contaDestino);
+}
